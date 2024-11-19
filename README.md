f0f8f3496ca19b3559d4ff572acc07c23de7fb29
mkusaka <hinoshita1992@gmail.com>
2024-11-19 10:11:54 -0800

    add readme

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 > convert `sitemap.xml` to `llms.txt`
 
-wip: making for my own use for dotenvx.com and will improve over time
+[![dotenvx](https://dotenvx.com/better-banner.png)](https://dotenvx.com)
+
+*generate `[llms.txt](https://dotenvx.com/llms.txt)`*–using your [`sitemap.xml`](https://dotenvx.com/sitemap.xml).
+
+* generate from `${url}/sitemap.xml`
+* exclude/include url paths
+* stdout to `llms.txt` file
+
+&nbsp;
+
+
+### Quickstart [![npm version](https://img.shields.io/npm/v/motdotla/llmstxt.svg)](https://www.npmjs.com/package/motdotla/llmstxt)
+
+Install and use it as a cli.
+
+```sh
+npm install llmstxt -g
+```
+```sh
+$ llmstxt gen https://yoursite.com/sitemap.xml
+```
+
+&nbsp;
--- src/cli/actions/gen.js
+++ src/cli/actions/gen.js
@@ -1,40 +1,102 @@
-const Sitemapper = require('sitemapper')
+const cheerio = require('cheerio')
+const picomatch = require('picomatch')
 const { request } = require('undici')
+const Sitemapper = require('sitemapper')
 const Turndown = require('turndown')
-const tds = new Turndown()
 
 const sitemap = new Sitemapper()
+const turndown = new Turndown()
 
 async function fetchHtml(url) {
   try {
     const { body } = await request(url)
     const rawHtml = await body.text()
     return rawHtml
-  } catch (error) {
-    console.error(`Error fetching HTML for ${url}:`, error.message)
+  } catch (_error) {
+    // console.error(`Error fetching HTML for ${url}:`, error.message)
     return null
   }
 }
 
-async function gen (url) {
+async function getTitle(html) {
   try {
-    const sites = await sitemap.fetch(url)
-    console.log(`Found ${sites.sites.length} URLs in the sitemap.`)
+    const $ = cheerio.load(html)
+    return $('head > title').text().trim()
+  } catch (_error) {
+    return null
+  }
+}
 
-    for (const site of sites.sites) {
-      console.log(`Fetching raw HTML for: ${site}`)
+async function getDescription(html) {
+  try {
+    const $ = cheerio.load(html)
 
-      const rawHtml = await fetchHtml(site)
-      if (rawHtml) {
-        const md = tds.turndown(rawHtml)
-        if (md) {
-          console.log(md)
-        }
+    // Check for <meta name="description">
+    let description = $('head > meta[name="description"]').attr('content')
+
+    // Fallback to <meta property="og:description">
+    if (!description) {
+      description = $('head > meta[property="og:description"]').attr('content')
+    }
+
+    // Fallback to <meta name="twitter:description">
+    if (!description) {
+      description = $('head > meta[name="twitter:description"]').attr('content')
+    }
+
+    return description
+  } catch (_error) {
+    return null
+  }
+}
+
+async function gen (sitemapUrl) {
+  const options = this.opts()
+
+  // include/exclude logic
+  const excludePaths = options.excludePath || []
+  const includePaths = options.includePath || []
+  const isExcluded = picomatch(excludePaths)
+  const isIncluded = picomatch(includePaths, { ignore: excludePaths })
+
+  let lines = []
+
+  try {
+    const sites = await sitemap.fetch(sitemapUrl)
+
+    for (const url of sites.sites) {
+      // path excluded - don't process it
+      if (isExcluded(url)) {
+        continue
       }
+
+      // path effectively excluded (by not being in the list of includes) - don't process it
+      if (includePaths.length > 0 && !isIncluded(url)) {
+        continue
+      }
+
+      const html = await fetchHtml(url)
+      if (!html) {
+        continue
+      }
+
+      const title = await getTitle(html)
+
+      if (!title) {
+        continue
+      }
+
+      const description = await getDescription(html)
+
+      lines.push(`- [${title}](${url}): ${description}`)
     }
   } catch (error) {
     console.error(`Error processing sitemap:`, error.message)
   }
+
+  const md = lines.join('\n')
+
+  console.log(md)
 }
 
 module.exports = gen
