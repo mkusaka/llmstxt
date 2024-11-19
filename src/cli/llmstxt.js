c39225ad5349979224ae3006ca9e98331ec151ca
Scott Motte <mot@mot.la>
2024-11-19 10:11:54 -0800

    add readme

--- src/cli/llmstxt.js
+++ src/cli/llmstxt.js
@@ -16,6 +16,8 @@ const genAction = require('./actions/gen')
 program.command('gen')
   .description('generate llms.txt')
   .argument('[url]', 'sitemap url', 'https://dotenvx.com/sitemap.xml')
+  .option('-ep, --exclude-path <excludePath...>', 'path(s) to exclude from generation (default: none)')
+  .option('-ip, --include-path <includePath...>', 'path(s) to include from generation (default: all)')
   .action(genAction)
 
 program.parse()
